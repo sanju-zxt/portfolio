307e20587aa544b80975f3c3007bc4b6cbbc1499
sanju-zxt <ssanju200610@gmail.com>
2026-05-01 20:42:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-
-// LOADER
-window.onload = () => {
-  document.getElementById("loader").style.display="none";
-};
-
-// TYPING
-const roles=["P","| AI Dev","| Data Engineer"];
-let i=0,j=0;
-
-function type(){
-  const el=document.getElementById("typing");
-  if(!el)return;
-
-  el.textContent=roles[i].substring(0,j++);
-  if(j>roles[i].length){j=0;i=(i+1)%roles.length;}
-  setTimeout(type,100);
-}
-type();
-
 // CURSOR
 const cursor=document.querySelector(".cursor");
 const blur=document.querySelector(".cursor-blur");
@@ -26,15 +6,15 @@ document.addEventListener("mousemove",(e)=>{
   cursor.style.left=e.clientX+"px";
   cursor.style.top=e.clientY+"px";
 
-  blur.style.left=e.clientX-60+"px";
-  blur.style.top=e.clientY-60+"px";
+  blur.style.left=e.clientX-30+"px";
+  blur.style.top=e.clientY-30+"px";
 });
 
 // MAGNETIC
 document.querySelectorAll(".magnetic").forEach(btn=>{
   btn.addEventListener("mousemove",e=>{
     const r=btn.getBoundingClientRect();
-    btn.style.transform=`translate(${(e.clientX-r.left-r.width/2)/5}px, ${(e.clientY-r.top-r.height/2)/5}px)`;
+    btn.style.transform=`translate(${(e.clientX-r.left-r.width/2)/6}px, ${(e.clientY-r.top-r.height/2)/6}px)`;
   });
 
   btn.addEventListener("mouseleave",()=>{
@@ -45,7 +25,6 @@ document.querySelectorAll(".magnetic").forEach(btn=>{
 // PARALLAX
 document.addEventListener("mousemove",(e)=>{
   const bg=document.querySelector(".parallax-layer");
-  if(!bg)return;
   bg.style.transform=`translate(${e.clientX/40}px,${e.clientY/40}px)`;
 });
 
