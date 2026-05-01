ae4a733a1d50c2adfd1cb5ca76f5be4964fc4cf4
sanju-zxt <ssanju200610@gmail.com>
2026-05-01 17:10:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-// SCROLL REVEAL
-const reveals=document.querySelectorAll(".reveal");
+// FETCH YOUR GITHUB PROJECTS
 
-window.addEventListener("scroll",()=>{
-  reveals.forEach(el=>{
-    const top=el.getBoundingClientRect().top;
-    if(top<window.innerHeight-100){
-      el.classList.add("active");
-    }
-  });
-});
+const username = "sanju-zxt";
 
-// 3D TILT
-document.querySelectorAll(".tilt").forEach(card=>{
-  card.addEventListener("mousemove",(e)=>{
-    let x=e.offsetX;
-    let y=e.offsetY;
-    let rotateX=-(y/10);
-    let rotateY=(x/10);
-    card.style.transform=`rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+fetch(`https://api.github.com/users/${username}/repos`)
+.then(res => res.json())
+.then(data => {
+
+  const container = document.getElementById("projects-container");
+
+  data.slice(0,3).forEach(repo => {
+    const div = document.createElement("div");
+
+    div.className = "project";
+
+    div.innerHTML = `
+      <h3>${repo.name}</h3>
+      <p>${repo.description || "No description"}</p>
+      <a href="${repo.html_url}" target="_blank">View →</a>
+    `;
+
+    container.appendChild(div);
   });
 
-  card.addEventListener("mouseleave",()=>{
-    card.style.transform="rotateX(0) rotateY(0)";
-  });
 });
